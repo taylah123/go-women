8fd0e94d8b27487ce4413a40be71c5a742f2333b
taylah123 <taylah.hasaballah@uqconnect.edu.au>
2015-03-24 09:44:26 +1000

    setup homepage

---
+++ temp_homepage.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ * Template Name: Custom Homepage
+ */
+
+include 'temp_header.php'; ?>
+<div class="temp-homepage">
+	<img src="" />
+	<p>Bla bla mission bla bla</p>
+</div>
+<?php get_footer(); ?>
