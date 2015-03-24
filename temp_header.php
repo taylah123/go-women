ef874cab65ed070ca1345502cf7581b094a9e9ef
taylah123 <taylah.hasaballah@uqconnect.edu.au>
2015-03-24 10:10:05 +1000

    fixed homepage font

--- temp_header.php
+++ temp_header.php
@@ -15,6 +15,7 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
