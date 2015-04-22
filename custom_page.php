6c455cc648ea4959bff9f02835b8fd5f3434013f
taylah123 <taylah.hasaballah@uqconnect.edu.au>
2015-04-22 18:38:44 +1000

    styled custom page

--- custom_page.php
+++ custom_page.php
@@ -9,29 +9,25 @@
  * @package WordPress
  * @subpackage Twenty_Fifteen
  * @since Twenty Fifteen 1.0
+ * Template Name: Custom Page
  */
 
 include 'temp_header.php'; ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div class="content_wrapper">
+				<?php
+				// Start the loop.
+				while ( have_posts() ) : the_post();
 
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
 
-			// Include the page content template.
-			get_template_part( 'content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		// End the loop.
-		endwhile;
-		?>
-
+				// End the loop.
+				endwhile;
+				?>
+			</div>
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
 
