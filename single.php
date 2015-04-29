3d0079911daef3548ec5433e7d0e4b16fb0d1f41
taylah123 <taylah.hasaballah@uqconnect.edu.au>
2015-04-29 21:16:35 +1000

    added comments

--- single.php
+++ single.php
@@ -36,6 +36,6 @@ include 'temp_header.php'; ?>
 		<p class="post_content"><?php echo $post->post_content; ?></p>
 		</div>
 	</div>
-<?php echo do_shortcode('[fbcomments][fbcomments url="http://peadig.com/wordpress-plugins/facebook-comments/" width="375" count="off" num="3" countmsg="wonderful comments!"]'); ?>
+<?php echo do_shortcode('[fbcomments width="700"]'); ?>
 	</div><!-- .content-area -->
 <?php get_footer(); ?>
