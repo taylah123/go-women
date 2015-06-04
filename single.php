5d3bc6c2b1f12a2b4edb51cc60d374d9d6c88d88
taylah123 <taylah.hasaballah@uqconnect.edu.au>
2015-06-04 17:07:56 +1000

    made ads white not yellow

--- single.php
+++ single.php
@@ -30,19 +30,15 @@ include 'temp_header.php'; ?>
 
 <div class="inner_content">
 <div class="ad_block_left">
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- 160 Post Page Banner -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:160px;height:600px"
-     data-ad-client="ca-pub-7995316478862697"
-     data-ad-slot="5653372367"></ins>
 </div>
 <div class="ad_block_right">
-<!-- 160 Post Page Banner -->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Banner Post Page -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:160px;height:600px"
+     style="display:block"
      data-ad-client="ca-pub-7995316478862697"
-     data-ad-slot="5653372367"></ins>
+     data-ad-slot="4316239961"
+     data-ad-format="auto"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
