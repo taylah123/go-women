576eb0edfa218238229f418b74352edd6d8965f9
taylah123 <taylah.hasaballah@uqconnect.edu.au>
2015-06-04 19:15:02 +1000

    made ads white not yellow

--- single.php
+++ single.php
@@ -29,18 +29,17 @@ include 'temp_header.php'; ?>
 <div class="posts_wrapper" id="post_page">
 
 <div class="inner_content">
-<div class="ad_block_left">
+<div class="ad_block_right">
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- 160 Post Page Banner -->
+<!-- Side Banner -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:160px;height:600px"
+     style="display:block"
      data-ad-client="ca-pub-7995316478862697"
-     data-ad-slot="5653372367"></ins>
+     data-ad-slot="2082627160"
+     data-ad-format="auto"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
-</div>
-<div class="ad_block_right">
 </div>
 		<h1><?php the_title(); ?></h1>
 	<?php if (is_single()) {
